c15d608e32c0b257b17b59d4c486b4494a725902
Mokhlaroyim <mokhlaroyim097@gmail.com>
2020-03-21 22:13:06 +0500

    feat: array-min-max-avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || array.length == 0)
+    return 0
+  var min = array[0]
+  for(var i=1; i<array.length; i++){
+    if(min>array[i])
+      min = array[i]
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array ==undefined || array.length==0)
+    return 0
+  var max = array[0]
+  for(var i=1; i<array.length; i++){
+    if(array[i]>max)
+      max = array[i]
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length==0)
+    return 0
+  var sum=0;
+  var i
+  for(i=0; i<array.length; i++){
+    sum+=array[i]
+  }
+  return sum/i;
 }
